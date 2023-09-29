0a0d562ecd47aefd4d324c95a3c80b4c7b9856d8
des1derx <ralo.ivan3002@gmail.com>
2023-09-29 23:18:55 +0300

    PostController,

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -1,4 +1,21 @@
-export const create = (req, res) => {
+import PostModel from "../models/Post.js";
+
+export const create = async (req, res) => {
   try {
-  } catch (err) {}
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось созлать статью",
+    });
+  }
 };
--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ import "dotenv/config";
 import cors from "cors";
 import { v4 as uuidv4, v4 } from "uuid";
 import mongoose from "mongoose";
-import { registerValidation, loginValidation } from "./validations.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
 import User from "./models/User.js";
 import * as UserController from "./Controllers/UserController.js";
+import * as PostController from "./Controllers/PostController.js";
 
 const app = express();
 const port = process.env.PORT || 7000;
@@ -25,9 +30,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/auth/login", loginValidation, UserController.login);
-
 app.get("/auth/me", checkAuth, UserController.register);
-
 app.post("/auth/register", registerValidation, UserController.getMe);
+
+app.post(
+  "/posts/create",
+  checkAuth,
+  postCreateValidation,
+  PostController.create
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
